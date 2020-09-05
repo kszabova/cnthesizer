b9c1143e94cfb03e30befb16d6fa7e6141196299
kristina <k.szabova98@gmail.com>
2020-09-05 13:59:33 +0200

    Play sounds using NAudio

--- CnthesizerForm.cs
+++ CnthesizerForm.cs
@@ -17,6 +17,9 @@ namespace Cnthesizer
 		private const int SAMPLE_RATE = 44100;
 		private const short BITS_PER_SAMPLE = 16;
 
+		private NAudio.Wave.WaveFileReader wave;
+		private NAudio.Wave.DirectSoundOut output;
+
 		public CnthesizerForm()
 		{
 			InitializeComponent();
@@ -24,7 +27,16 @@ namespace Cnthesizer
 
 		private void CnthesizerForm_KeyDown(object sender, KeyEventArgs e)
 		{
-			byte[] wave = Wave.A;
+			wave = new NAudio.Wave.WaveFileReader("output.wav");
+			output = new NAudio.Wave.DirectSoundOut();
+			output.Init(new NAudio.Wave.WaveChannel32(wave));
+			output.Play();
+			//byte[] wave = Wave.A;
+			//switch (e.KeyCode)
+			//{
+			//	case Keys.A: { wave = Wave.Waves[0]; break; }
+			//}
+
 			//short[] wave = new short[SAMPLE_RATE];
 			//byte[] binaryWave = new byte[SAMPLE_RATE * sizeof(short)];
 			//float frequency = 440f;
@@ -34,27 +46,42 @@ namespace Cnthesizer
 			//	wave[i] = Convert.ToInt16(short.MaxValue * Math.Sin(((Math.PI * 2 * frequency) / SAMPLE_RATE) * i));
 			//}
 			//Buffer.BlockCopy(wave, 0, binaryWave, 0, wave.Length * sizeof(short));
-			using (MemoryStream memoryStream = new MemoryStream())
-			using (BinaryWriter binaryWriter = new BinaryWriter(memoryStream))
-			{
-				short blockAlign = BITS_PER_SAMPLE / 8;
-				int subchunkTwoSize = SAMPLE_RATE * blockAlign;
-				binaryWriter.Write(new[] { 'R', 'I', 'F', 'F' });
-				binaryWriter.Write(36 + subchunkTwoSize);
-				binaryWriter.Write(new[] { 'W', 'A', 'V', 'E', 'f', 'm', 't', ' ' });
-				binaryWriter.Write(16);
-				binaryWriter.Write((short)1);
-				binaryWriter.Write((short)1);
-				binaryWriter.Write(SAMPLE_RATE);
-				binaryWriter.Write(SAMPLE_RATE * blockAlign);
-				binaryWriter.Write(blockAlign);
-				binaryWriter.Write(BITS_PER_SAMPLE);
-				binaryWriter.Write(new[] { 'd', 'a', 't', 'a' });
-				binaryWriter.Write(subchunkTwoSize);
-				binaryWriter.Write(wave);
-				memoryStream.Position = 0;
-				new SoundPlayer(memoryStream).Play();
-			}
+
+			//using (MemoryStream memoryStream = new MemoryStream())
+			//using (BinaryWriter binaryWriter = new BinaryWriter(memoryStream))
+			//{
+			//	short blockAlign = BITS_PER_SAMPLE / 8;
+			//	int subchunkTwoSize = SAMPLE_RATE * blockAlign;
+			//	binaryWriter.Write(new[] { 'R', 'I', 'F', 'F' });
+			//	binaryWriter.Write(36 + subchunkTwoSize);
+			//	binaryWriter.Write(new[] { 'W', 'A', 'V', 'E', 'f', 'm', 't', ' ' });
+			//	binaryWriter.Write(16);
+			//	binaryWriter.Write((short)1);
+			//	binaryWriter.Write((short)1);
+			//	binaryWriter.Write(SAMPLE_RATE);
+			//	binaryWriter.Write(SAMPLE_RATE * blockAlign);
+			//	binaryWriter.Write(blockAlign);
+			//	binaryWriter.Write(BITS_PER_SAMPLE);
+			//	binaryWriter.Write(new[] { 'd', 'a', 't', 'a' });
+			//	binaryWriter.Write(subchunkTwoSize);
+			//	binaryWriter.Write(wave);
+			//	memoryStream.Position = 0;
+			//	soundPlayer.Stream = memoryStream;
+			//	soundPlayer.Load();
+			//	soundPlayer.Play();
+			//	memoryStream.Seek(0, SeekOrigin.Begin);
+			//	FileStream fs = File.Create("output.wav");
+			//	byte[] buf = new byte[65536];
+			//	int len = 0;
+			//	while ((len = memoryStream.Read(buf, 0, 65536)) > 0)
+			//	{
+			//		fs.Write(buf, 0, len);
+			//	}
+			//	fs.Close();
+			//}
+
+			//soundPlayer.SoundLocation = "output.wav";
+			//soundPlayer.Play();
 		}
 	}
 }
--- Wave.cs
+++ Wave.cs
@@ -13,15 +13,27 @@ namespace Cnthesizer
 		private static readonly int SAMPLE_RATE = 44100;
 		private static readonly short BITS_PER_SAMPLE = 16;
 
-		public static byte[] A = CreateWave(Frequency.A);
+		public static List<byte[]> Waves = new List<byte[]> { };
 
-		private static byte[] CreateWave(Frequency frequencyCode)
+		public Wave()
 		{
-			short[] wave = new short[SAMPLE_RATE];
-			byte[] binaryWave = new byte[SAMPLE_RATE * sizeof(short)];
+			foreach (Frequency frequency in Enum.GetValues(typeof(Frequency)))
+			{
+				byte[] wave = CreateWave(frequency);
+				Waves.Add(wave);
+			}
+		}
+
+
+		public static byte[] A = CreateWave(Frequency.B);
+
+		private static byte[] CreateWave(Frequency frequencyCode, int length = 44100)
+		{
+			short[] wave = new short[length];
+			byte[] binaryWave = new byte[length * sizeof(short)];
 			float frequency = Frequencies.Freqs[(int)frequencyCode];
 
-			for (int i = 0; i < SAMPLE_RATE; ++i)
+			for (int i = 0; i < length; ++i)
 			{
 				wave[i] = Convert.ToInt16(short.MaxValue * Math.Sin(((Math.PI * 2 * frequency) / SAMPLE_RATE) * i));
 			}
