580c5b6b4ca581e21fbdf38d935bc21aa2126b65
kristina <k.szabova98@gmail.com>
2020-09-07 23:47:53 +0200

    Add method for writing a .wav file

--- Wave.cs
+++ Wave.cs
@@ -53,5 +53,37 @@ namespace Cnthesizer
 			Buffer.BlockCopy(wave, 0, binaryWave, 0, wave.Length * sizeof(short));
 			return binaryWave;
 		}
+
+		public static void SaveToFile(string path, byte[] wave, int length, int sampleRate, short bitsPerSample, short channels)
+		{
+			using (MemoryStream memoryStream = new MemoryStream())
+			using (BinaryWriter binaryWriter = new BinaryWriter(memoryStream))
+			{
+				short blockAlign = (short)(bitsPerSample / 8);
+				int subchunkTwoSize = length * channels * blockAlign;
+				binaryWriter.Write(new[] { 'R', 'I', 'F', 'F' });
+				binaryWriter.Write(36 + subchunkTwoSize);
+				binaryWriter.Write(new[] { 'W', 'A', 'V', 'E', 'f', 'm', 't', ' ' });
+				binaryWriter.Write(16);
+				binaryWriter.Write((short)1);
+				binaryWriter.Write(channels);
+				binaryWriter.Write(SAMPLE_RATE);
+				binaryWriter.Write(SAMPLE_RATE * channels * blockAlign);
+				binaryWriter.Write(channels * blockAlign);
+				binaryWriter.Write(BITS_PER_SAMPLE);
+				binaryWriter.Write(new[] { 'd', 'a', 't', 'a' });
+				binaryWriter.Write(subchunkTwoSize);
+				binaryWriter.Write(wave);
+				memoryStream.Seek(0, SeekOrigin.Begin);
+				FileStream fs = File.Create(path);
+				byte[] buf = new byte[65536];
+				int len = 0;
+				while ((len = memoryStream.Read(buf, 0, 65536)) > 0)
+				{
+					fs.Write(buf, 0, len);
+				}
+				fs.Close();
+			}
+		}
 	}
 }
