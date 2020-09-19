0509ece9294f9249a7bea6374b5489b59951f53b
kristina <k.szabova98@gmail.com>
2020-09-19 22:05:27 +0200

    Change how chords are generated

--- Chord.cs
+++ Chord.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace Cnthesizer
 {
+	struct Scale
+	{
+		public bool Major;
+		public Pitch Base;
+		public Scale(bool major, Pitch baseTone)
+			=> (Major, Base) = (major, baseTone);
+	}
+
 	public enum ChordName
 	{
-		ilowMaj, iiMaj, iiiMaj, ivMaj, vMaj, viMaj, viiMaj, ihighMaj,
-		ilowMin, iiMin, iiiMin, ivMin, vMin, viMin, viiMin, ihighMin
+		ilow, ii, iii, iv, v, vi, vii, ihigh
 	}
 
 	class Chord
@@ -21,31 +28,35 @@ namespace Cnthesizer
 				=> (this.first, this.second, this.third) = (first, second, third);
 		}
 
-		private Dictionary<ChordName, ChordShiftFromBase> chordShifts = new Dictionary<ChordName, ChordShiftFromBase>
+		private Dictionary<ChordName, ChordShiftFromBase> MajChordShifts = new Dictionary<ChordName, ChordShiftFromBase>
 		{
-			{ChordName.ilowMaj, new ChordShiftFromBase(0, 4, 7) },
-			{ChordName.iiMaj, new ChordShiftFromBase(2, 5, 9) },
-			{ChordName.iiiMaj, new ChordShiftFromBase(4, 7, 11) },
-			{ChordName.ivMaj, new ChordShiftFromBase(5, 9, 12) },
-			{ChordName.vMaj, new ChordShiftFromBase(7, 11, 14) },
-			{ChordName.viMaj, new ChordShiftFromBase(9, 12, 16) },
-			{ChordName.viiMaj, new ChordShiftFromBase(11, 14, 17) },
-			{ChordName.ihighMaj, new ChordShiftFromBase(12, 16, 19) },
-			{ChordName.ilowMin, new ChordShiftFromBase(0, 3, 7) },
-			{ChordName.iiMin, new ChordShiftFromBase(2, 5, 8) },
-			{ChordName.iiiMin, new ChordShiftFromBase(3, 7, 10) },
-			{ChordName.ivMin, new ChordShiftFromBase(5, 8, 12) },
-			{ChordName.vMin, new ChordShiftFromBase(7, 10, 14) },
-			{ChordName.viMin, new ChordShiftFromBase(8, 12, 15) },
-			{ChordName.viiMin, new ChordShiftFromBase(10, 14, 17) },
-			{ChordName.ihighMin, new ChordShiftFromBase(12, 15, 19) }
+			{ChordName.ilow, new ChordShiftFromBase(0, 4, 7) },
+			{ChordName.ii, new ChordShiftFromBase(2, 5, 9) },
+			{ChordName.iii, new ChordShiftFromBase(4, 7, 11) },
+			{ChordName.iv, new ChordShiftFromBase(5, 9, 12) },
+			{ChordName.v, new ChordShiftFromBase(7, 11, 14) },
+			{ChordName.vi, new ChordShiftFromBase(9, 12, 16) },
+			{ChordName.vii, new ChordShiftFromBase(11, 14, 17) },
+			{ChordName.ihigh, new ChordShiftFromBase(12, 16, 19) }
+		};
+
+		private Dictionary<ChordName, ChordShiftFromBase> MinChordShifts = new Dictionary<ChordName, ChordShiftFromBase>
+		{
+			{ChordName.ilow, new ChordShiftFromBase(0, 3, 7) },
+			{ChordName.ii, new ChordShiftFromBase(2, 5, 8) },
+			{ChordName.iii, new ChordShiftFromBase(3, 7, 10) },
+			{ChordName.iv, new ChordShiftFromBase(5, 8, 12) },
+			{ChordName.v, new ChordShiftFromBase(7, 10, 14) },
+			{ChordName.vi, new ChordShiftFromBase(8, 12, 15) },
+			{ChordName.vii, new ChordShiftFromBase(10, 14, 17) },
+			{ChordName.ihigh, new ChordShiftFromBase(12, 15, 19) }
 		};
 
 		public List<Pitch> Tones { get; }
-		private readonly Pitch scale;
+		private readonly Scale scale;
 		private readonly ChordName chordName;
 
-		public Chord(Pitch scale, ChordName chordName)
+		public Chord(Scale scale, ChordName chordName)
 		{
 			this.scale = scale;
 			this.chordName = chordName;
@@ -54,10 +65,12 @@ namespace Cnthesizer
 
 		public List<Pitch> GetChord()
 		{
+			Dictionary<ChordName, ChordShiftFromBase> chordShifts = scale.Major ? MajChordShifts : MinChordShifts;
+
 			List<Pitch> chord = new List<Pitch> { };
-			chord.Add(PitchSelector.ShiftPitchBySemitones(scale, chordShifts[chordName].first));
-			chord.Add(PitchSelector.ShiftPitchBySemitones(scale, chordShifts[chordName].third));
-			chord.Add(PitchSelector.ShiftPitchBySemitones(scale, chordShifts[chordName].third));
+			chord.Add(PitchSelector.ShiftPitchBySemitones(scale.Base, chordShifts[chordName].first));
+			chord.Add(PitchSelector.ShiftPitchBySemitones(scale.Base, chordShifts[chordName].third));
+			chord.Add(PitchSelector.ShiftPitchBySemitones(scale.Base, chordShifts[chordName].third));
 			return chord;
 		}
 	}
